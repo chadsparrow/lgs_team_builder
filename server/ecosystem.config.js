f8a5dea072c1ba9be64c656f5edf0b9bab5edaaa
Chad Sparrow <csparrow@sugoi.com>
2019-07-21 00:31:56 -0700

    simplified vue docker and pm2 ecosystem

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,25 +1,21 @@
 module.exports = {
   apps: [
     {
-      name: "TEAM-BUILDER API",
-      script: "server.js",
+      name: 'TEAM-BUILDER API',
+      script: 'server.js',
 
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       instances: 4,
       autorestart: true,
       watch: false,
-      ignore_watch: ["logs", "node_modules"],
+      ignore_watch: ['logs', 'node_modules'],
       watch_options: {
         followSymlinks: false
       },
-      max_memory_restart: "1G",
-      env_development: {
-        NODE_ENV: "development",
-        MONGO_INITDB_ROOT_DB: "teambuilder-dev"
-      },
+      max_memory_restart: '1G',
       env_production: {
-        NODE_ENV: "production",
-        MONGO_INITDB_ROOT_DB: "teambuilder-prod"
+        NODE_ENV: 'production',
+        MONGO_INITDB_ROOT_DB: 'teambuilder-prod'
       }
     }
   ]
