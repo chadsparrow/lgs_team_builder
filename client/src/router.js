7e59c85d025debc38885dd657d699a280545b0d2
Chad Sparrow <csparrow@sugoi.com>
2019-07-21 00:14:06 -0700

    fixed vue linting and removed dev pm2

--- client/src/router.js
+++ client/src/router.js
@@ -1,27 +1,27 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      component: () => import("./views/About.vue")
+      path: '/about',
+      name: 'about',
+      component: () => import('./views/About.vue')
     },
     {
-      path: "/contact",
-      name: "contact",
-      component: () => import("./views/ContactUs.vue")
+      path: '/contact',
+      name: 'contact',
+      component: () => import('./views/ContactUs.vue')
     }
   ]
 });
--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-const app = require("./app");
-const config = require("config");
-const winston = require("winston");
+const app = require('./app');
+const config = require('config');
+const winston = require('winston');
 
 // configures server port
-const APP_PORT = config.get("app.port") || 5001;
+const APP_PORT = config.get('app.port') || 5001;
 // configures server to listen to configured server port above
 app.listen(APP_PORT, () => winston.info(`Team Builder listening on port ${APP_PORT}...`));
