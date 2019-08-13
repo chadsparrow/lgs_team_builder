8329c4a1dc6d8883edafc63847693d01b5530096
chadsparrow <csparrow@sugoi.com>
2019-08-13 14:06:53 -0700

    added logger

---
+++ middleware/logger.js
@@ -0,0 +1,19 @@
+const winston = require('winston');
+require('winston-daily-rotate-file');
+
+const logger = new winston.Logger({
+  transports: [
+    new winston.transports.Console({
+      timestamp: new Date().toISOString(),
+      colorize: true
+    })
+  ]
+});
+
+// setup winston
+logger.add(winston.transports.DailyRotateFile, {
+  filename: './logs/teambuilder-%DATE%.log',
+  maxFiles: '14d'
+});
+
+module.exports = logger;
--- server.js
+++ server.js
@@ -1,12 +1,55 @@
+const express = require('express');
+const Joi = require('@hapi/joi');
+Joi.objectId = require('joi-objectid')(Joi);
+
 const config = require('config');
-const winston = require('winston');
-const app = require('./app');
+const logger = require('./middleware/logger');
+require('express-async-errors');
+
+const app = express();
+
+const DB_USER = config.get('database.user');
+const DB_PASS = config.get('database.pass');
+const DB_ADMINSOURCE = config.get('database.adminSource');
+const DB_DB = config.get('database.db');
+const DB_PORT = config.get('database.port') || 27018;
+let DB_URI = '';
+let DB_HOST = '';
+
+if (config.get('env') === 'test') {
+  DB_URI = 'localhost';
+} else {
+  DB_URI = 'mongo';
+}
+
+DB_HOST = `mongodb://${DB_USER}:${DB_PASS}@${DB_URI}:${DB_PORT}/${DB_DB}?authSource=${DB_ADMINSOURCE}`;
+require('./startup/db')(DB_HOST);
+
+// handle all uncaught expceptions
+process.on('uncaughtException', ex => {
+  logger.error(ex.message, ex);
+  process.exit(1);
+});
+
+// throw an exception to uncaught exception handler if unhandle promise rejection is found.
+process.on('unhandledRejection', ex => {
+  throw ex;
+});
+
+// Check if jwtPrivateKey env variable is set
+if (!config.get('jwtPrivateKey')) {
+  logger.error('FATAL ERROR: jwtPrivateKey is not defined.');
+  process.exit(1);
+}
+
+require('./startup/routes')(app);
 
 // configures server port
-const APP_PORT = config.get('app.port') || 5001;
+const PORT = config.get('app.port') || 5001;
 
 // configures server to listen to configured server port above
-const server = app.listen(APP_PORT, () =>
-  winston.info(`Team Builder (${process.env.NODE_ENV}) now listening on port ${APP_PORT}...`)
+app.listen(PORT, () =>
+  logger.info(`Team Builder (${process.env.NODE_ENV}) now listening on port ${PORT}...`)
 );
-module.exports = server;
+
+module.exports = app;
