3144cc79ba342a4a93f387ce01f284cb03ff142a
chadsparrow <csparrow@sugoi.com>
2019-11-04 12:21:20 -0800

    added xss prevention

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('express-async-errors');
 const app = express();
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 // const cors = require('cors');
 const config = require('config');
 const Joi = require('@hapi/joi');
@@ -15,6 +16,7 @@ const requestLogger = require('./middleware/requestLogger');
 // Set up express, security
 app.use(helmet());
 app.use(mongoSanitize());
+app.use(xss());
 // app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
