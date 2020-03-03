74cad241fb5f9d94d7aa267cb7093d009d226a45
chadsparrow <csparrow@sugoi.com>
2020-03-03 14:50:50 -0800

    moved test to unit folder

--- __tests__/unit/server/utils/sum.test.js
+++ __tests__/unit/server/utils/sum.test.js
@@ -2,7 +2,7 @@
  * @jest-environment node
  */
 
-const sum = require('../../server/utils/sum');
+const sum = require('../../../../server/utils/sum');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
